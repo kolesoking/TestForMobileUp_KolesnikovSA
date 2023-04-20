f6ec0cf0009b6c74b03c51fc3557b38ceab8a960
kolesoking <kolesnikov.sergey.al@gmail.com>
2023-04-20 12:37:27 +0300

    Initial Commit

---
+++ TestForMobileUp_KolesnikovSA/Appearence/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  TestForMobileUp_KolesnikovSA
+//
+//  Created by Sergey Kolesnikov on 20.04.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = LoginViewController()
+        return true
+    }
+}
+
---
+++ TestForMobileUp_KolesnikovSA/MainFlow/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  TestForMobileUp_KolesnikovSA
+//
+//  Created by Sergey Kolesnikov on 20.04.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
